1dcb1a1769b180b5f1c6b2994c79ed4ddf21f431
Jorome Marcon <joromemarcon@gmail.com>
2021-06-25 17:23:53 -0700

    Created method skeleton

---
+++ src/src/Term.java
@@ -0,0 +1,58 @@
+package src;
+
+public class Term {
+    /*
+        Term x = new Term(); //creates an instance of Term called x
+        x contains coefficient and exponent
+
+
+     */
+    public Term(){
+        int coefficient;
+        int exponent;
+    }
+
+    public Term(int coefficient, int exponent){
+
+    }
+    //constructor with Term as parameter
+    public Term(Term other){
+
+    }
+
+    //constructor with string parameter
+    public Term(String term){
+
+    }
+
+    public void setCoefficient(int value){
+
+    }
+    public void setExponent(int value){
+
+    }
+
+    public int getCoefficient(){
+
+    }
+    public int getExponent(){
+
+    }
+
+
+    public int compareTo(Object other){
+
+    }
+
+    //toString converts linkedList into string
+    public String toString(){
+
+    }
+
+    public Term plus(Term other){
+
+    }
+    public Term sumOf(Term termA, Term term8){
+
+    }
+}
